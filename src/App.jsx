bb8139703278abbd0f8c03122df7ff59663466b0
Tpepper2001 <oluwatoyinishola2024@gmail.com>
2025-12-14 14:36:07 +0100

    Update Supabase URL and key in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,8 +10,8 @@ import './App.css'; // Importing the CSS file
 // ==========================================
 // CONFIGURATION
 // ==========================================
-const supabaseUrl = "YOUR_SUPABASE_URL";
-const supabaseKey = "YOUR_SUPABASE_ANON_KEY";
+const supabaseUrl = "https://wzgjecyzxnohieoijcwr.supabase.co";
+const supabaseKey = "";eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind6Z2plY3l6eG5vaGllb2lqY3dyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU3MDE4NjcsImV4cCI6MjA4MTI3Nzg2N30._XOGAVxWr1rxND0gUP8AEcIDoU9v5xyqnnl8mB4eMNs
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 // ==========================================
